445254a7af6f0baff6316fc8a5d00d7401ee44a9
kane.ch <yiying5@gmail.com>
2014-07-24 18:29:12 +0800

    add hession

---
+++ src/test/java/cn/kane/rpc/test/HessionClientTest.java
@@ -0,0 +1,39 @@
+package cn.kane.rpc.test;
+
+import java.net.MalformedURLException;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import cn.kane.service.IDemoService;
+
+import com.caucho.hessian.client.HessianProxyFactory;
+
+public class HessionClientTest extends TestCase{
+	
+	private IDemoService demoService ;
+	private HessianProxyFactory factory = new HessianProxyFactory();
+	
+	@Test
+	public void singHessionTest() throws MalformedURLException, ClassNotFoundException{
+		String url = "http://localhost:8080/dubbo-demo/singhession";
+		demoService = (IDemoService) this.getService(url,null);
+		demoService.sayHi();
+	}
+	
+	@Test
+	public void testHessionWithSpring() throws MalformedURLException, ClassNotFoundException{
+		String url = "http://localhost:8080/dubbo-demo/springhession/demoServiceInSpringHession";
+		demoService = (IDemoService) this.getService(url,IDemoService.class);
+		demoService.sayHi();
+	}
+	
+	private Object getService(String serviceProviderUrl,Class<?> clazz) throws MalformedURLException, ClassNotFoundException{
+		if(null == clazz){
+			return factory.create(serviceProviderUrl) ;
+		}else{
+			return factory.create(clazz,serviceProviderUrl) ;
+		}
+	}
+}
